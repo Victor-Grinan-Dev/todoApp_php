cf92360e465407023427953ac623503716e6b12b
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-04-18 10:24:22 +0300

    queries

--- .dist/queries.sql
+++ .dist/queries.sql
@@ -1,7 +1,15 @@
-create table taskslist(
+
+drop database if exists todoapp;
+create database todoapp;
+use todoapp;
+
+create table tasks(
     id integer primary key,
     task varchar(20) not null,
-    explanation varchar(20),
     finished boolean,
-    responsible varchar(20)
 );
+
+
+insert into tasks values(1, 'clean room', false);
+insert into tasks values(2, 'do shoppings', false);
+insert into tasks values(3, 'take out trash', false);
