c1d7f115c8737a994d9022e849edd15738ea11f0
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-04-18 16:47:44 +0300

    reset files

--- connection.php
+++
@@ -1,8 +0,0 @@
- <?php 
-$user = "root";
-$password = "lionpass";
-$host = "localhost";
-$name = "todolist";
-
-$conn = @mysqli_connect($host, $user, $password, $db_name) or die("couldn't connect" . mysqli_connect_error());
- ?>
